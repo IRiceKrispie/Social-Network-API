ad9d54e5838cd80285f1cbb99366f4bc93947ff2
IRiceKrispie <joseph.sandoval.cs6@gmail.com>
2023-02-03 00:09:01 -0600

    finished Thought and reaction models

---
+++ models/Thought.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min: 1,
+            max: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        userName: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+//virtual for reactionCount
+thoughtSchema
+    .virtual('reactionCount')
+    .get(function (){
+        return `${this.reactions.length}`;
+    })
+    .set(function(v) {
+        const length = v;
+        this.set({length});
+    });
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
